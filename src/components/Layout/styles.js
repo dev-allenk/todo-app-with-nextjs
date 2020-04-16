26a2c9418b7d9752b8d4d473b3dfaed97e7fbf6e
allenk <dev.allenk@gmail.com>
2020-04-16 20:14:40 +0900

    feat: 레이아웃에 border 추가

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -11,6 +11,8 @@ const Wrapper = styled.div`
 const Layout = styled.div`
   width: 500px;
   min-height: 500px;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
 `;
 
 export default { Layout, Wrapper };
